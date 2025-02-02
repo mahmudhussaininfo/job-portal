cb9d920a30bff9e0a88f51724563e9a144dd00c8
mahmud <mahmudhussaininfo@gmail.com>
2025-02-02 18:18:45 +0600

    webhooks update

--- api/controller/webhooks.js
+++ api/controller/webhooks.js
@@ -9,8 +9,8 @@ export const clerkWebhooks = async (req, res) => {
     // verify headers
     await webhook.verify(JSON.stringify(req.body), {
       "svix-id": req.headers["svix-id"],
-      "svix-signature": req.headers["svix-signature"],
       "svix-timestamp": req.headers["svix-timestamp"],
+      "svix-signature": req.headers["svix-signature"],
     });
 
     // getting data from body
@@ -21,36 +21,35 @@ export const clerkWebhooks = async (req, res) => {
       case "user.created": {
         const userData = {
           _id: data.id,
-          email: data.email_address[0].email_address,
+          email: data.email_addresses[0].email_address,
           name: data.first_name + " " + data.last_name,
           image: data.image_url,
           resume: "",
         };
         await User.create(userData);
-        res.json({ message: "user created" });
+        res.json({});
         break;
       }
       case "user.updated": {
         const userData = {
-          email: data.email_address[0].email_address,
+          email: data.email_addresses[0].email_address,
           name: data.first_name + " " + data.last_name,
           image: data.image_url,
         };
-        await User.findByIdAndUpdate(data.id, userData, { new: true });
-        res.json({ message: "user updated" });
+        await User.findByIdAndUpdate(data.id, userData);
+        res.json({});
         break;
       }
       case "user.deleted": {
         await User.findByIdAndDelete(data.id);
-        res.json({ message: "user deleted" });
+        res.json({});
         break;
       }
-
       default:
         break;
     }
   } catch (error) {
     console.log(error.message);
-    res.status(500).json({ status: "faild", message: "webhooks Error" });
+    res.json({ success: "false", message: "webhooks Error" });
   }
 };
