128b9d329bd8af751f37f80ac9f53f141fc0e3b9
mahmud <mahmudhussaininfo@gmail.com>
2025-02-17 13:24:44 +0600

    update

--- api/server.js
+++ api/server.js
@@ -17,7 +17,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:3000", ""],
+    origin: [
+      "http://localhost:3000",
+      "https://job-portal-client-six-lime.vercel.app",
+    ],
     credentials: true,
   })
 );
