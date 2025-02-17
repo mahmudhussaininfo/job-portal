9a86280818d78950ffbdad9d6ca115855ab4db8e
mahmud <mahmudhussaininfo@gmail.com>
2025-02-17 09:46:46 +0600

    origin change

--- api/server.js
+++ api/server.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://job-portal-9r7w-client.vercel.app",
+    origin: "http://localhost:3000",
     credentials: true,
   })
 );
